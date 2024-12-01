5d017693a5cb4eb63ad81c67f07d1c29dcc6a304
the-all-good <benjo@dcsi.net.au>
2024-12-01 12:42:57 +1100

    Initial Setup

---
+++ src/input.php
@@ -0,0 +1,30 @@
+<?php
+namespace Input;
+
+class Input{
+    public $url;
+    public $output;
+    function __construct($url){
+        $this->url = $url;
+        $cookie = "session=" . parse_ini_file('.env')['SESSION'];
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_COOKIE, $cookie);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $output = curl_exec($ch);
+        curl_close($ch);
+        $this->output = $output;
+    }
+    public function get_input(){
+        return $this->output;
+    }
+    public function split_by_newlines(){
+        $split = explode("\n", $this->get_input());
+        foreach($split as $key => $line){
+            if($line === ""){
+                unset($split[$key]);
+            }
+        }
+        return $split;
+    }
+}
