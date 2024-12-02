51af2a58261153a2ccff4ffbb93f9d9df1df1ced
the-all-good <benjo@dcsi.net.au>
2024-12-02 20:36:34 +1100

    fixed d2p2

--- Day02/part2.php
+++ Day02/part2.php
@@ -5,59 +5,44 @@ $level = new Input(2);
 $total = 0;
 
 $input = $level->split_by_newlines();
-// $input = ['7 6 4 2 1',
-// '1 2 7 8 9',
-// '9 7 6 2 1',
-// '1 3 2 4 5',
-// '8 6 4 4 1',
-// '1 3 6 7 9'];
 
 foreach ($input as $line) {
-    $check = check_safe(explode(" ",$line));
+    $check = remove_bad(explode(" ",$line));
     if($check){
         $total += 1;
     }
 }
 echo $total . "\n";
 
+function remove_bad(array $nums){
+    foreach ($nums as $key => $num){
+        $clone = $nums;
+        unset($clone[$key]);
+        $attempt = check_safe(array_values($clone));
+        if ($attempt){
+            return true;
+        }
+    }
+    print_r($nums);
+    return false;
+}
+
 function check_safe(array $nums){
     $direction = "";
-    $faults = 0;
-    $test = $nums;
     for ($i=1; $i < count($nums); $i++) { 
-
-        if ($faults > 1){
-            print_r($test);
-            print_r($nums);
-            return false;
-        }
-
         $diff = $nums[$i -1] - $nums[$i];
         
         if ($direction == "") {
-            $direction = $diff == abs($diff) ? "down" : "up";
+            $direction = $diff == abs($diff) ? "up" : "down";
         }
         
         if ($diff == 0 || abs($diff) > 3){
-            echo $nums[$i - 1] . "\n";
-            unset($nums[$i -1]);
-            $faults += 1;
-            $direction = "";
-            $nums = array_values($nums);
-            $i = 0;
-            continue;
+            return false;
         }
         
-        if (($diff == abs($diff) && $direction == "up") || ($diff != abs($diff) && $direction == "down")){
-            echo $nums[$i - 1] . " {$direction}  {$diff} {$nums[$i]}" .  "\n";
-            unset($nums[$i -1]);
-            $faults += 1;
-            $direction = "";
-            $nums = array_values($nums);
-            $i = 0;
-            continue;
+        if (($diff == abs($diff) && $direction == "down") || ($diff != abs($diff) && $direction == "up")){
+            return false;
         }
     }
-
-    return $nums;
+    return true;
 }
